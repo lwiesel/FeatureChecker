80d966f1f2eacaa5e0fe09fc59618d03a6ea10cb
Laurent Wiesel <laurent.wiesel@ar-developpement.fr>
2015-02-23 23:27:19 +0100

    Revert array notations to maintain php5.3 compatibility

--- spec/FeatureCheckerSpec.php
+++ spec/FeatureCheckerSpec.php
@@ -7,7 +7,7 @@ use Prophecy\Argument;
 
 class FeatureCheckerSpec extends ObjectBehavior
 {
-    protected $proper_config = ['test' => true, 'test-2' => false];
+    protected $proper_config = array('test' => true, 'test-2' => false);
 
     function it_is_initializable()
     {
@@ -18,34 +18,34 @@ class FeatureCheckerSpec extends ObjectBehavior
     function it_should_throw_an_exception_when_parameters_are_not_an_array()
     {
         $parameters = 'string';
-        $this->shouldThrow('\InvalidArgumentException')->during('__construct', [$parameters]);
+        $this->shouldThrow('\InvalidArgumentException')->during('__construct', array($parameters));
 
         $parameters = 0;
-        $this->shouldThrow('\InvalidArgumentException')->during('__construct', [$parameters]);
+        $this->shouldThrow('\InvalidArgumentException')->during('__construct', array($parameters));
 
         $parameters = true;
-        $this->shouldThrow('\InvalidArgumentException')->during('__construct', [$parameters]);
+        $this->shouldThrow('\InvalidArgumentException')->during('__construct', array($parameters));
     }
 
     function it_should_throw_an_exception_when_parameters_are_not_an_array_of_booleans()
     {
-        $parameters = ['string', 0, true];
-        $this->shouldThrow('\InvalidArgumentException')->during('__construct', [$parameters]);
+        $parameters = array('string', 0, true);
+        $this->shouldThrow('\InvalidArgumentException')->during('__construct', array($parameters));
 
-        $parameters = ['string', 'string'];
-        $this->shouldThrow('\InvalidArgumentException')->during('__construct', [$parameters]);
+        $parameters = array('string', 'string');
+        $this->shouldThrow('\InvalidArgumentException')->during('__construct', array($parameters));
 
-        $parameters = [0, 0, 0];
-        $this->shouldThrow('\InvalidArgumentException')->during('__construct', [$parameters]);
+        $parameters = array(0, 0, 0);
+        $this->shouldThrow('\InvalidArgumentException')->during('__construct', array($parameters));
     }
 
     function it_should_throw_an_exception_when_parameters_are_not_indexed_by_strings()
     {
-        $parameters = ['test' => true, 2 => false];
-        $this->shouldThrow('\InvalidArgumentException')->during('__construct', [$parameters]);
+        $parameters = array('test' => true, 2 => false);
+        $this->shouldThrow('\InvalidArgumentException')->during('__construct', array($parameters));
 
-        $parameters = [true, false];
-        $this->shouldThrow('\InvalidArgumentException')->during('__construct', [$parameters]);
+        $parameters = array(true, false);
+        $this->shouldThrow('\InvalidArgumentException')->during('__construct', array($parameters));
     }
 
     function it_should_not_throw_an_exception_when_parameters_string_indexed_booleans()
@@ -56,7 +56,7 @@ class FeatureCheckerSpec extends ObjectBehavior
     function it_should_throw_an_exception_when_feature_does_not_exist()
     {
         $this->beConstructedWith($this->proper_config);
-        $this->shouldThrow('LWI\FeatureChecker\Exception\FeatureNotDefinedException')->during('isFeatureEnabled', ['undefined-feature']);
+        $this->shouldThrow('LWI\FeatureChecker\Exception\FeatureNotDefinedException')->during('isFeatureEnabled', array('undefined-feature'));
     }
 
     function it_should_say_if_a_feature_is_enabled()
